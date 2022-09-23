596b3f6bda8f505698861b910e39e18c20691273
FireIsGood <fireisadequate@gmail.com>
2022-09-23 00:12:10 -0700

    Update about page script

--- scripts/cards.js
+++ scripts/cards.js
@@ -17,20 +17,3 @@ if (is_touch_device) {
     });
   }
 }
-
-const glitchTexts = document.querySelectorAll(".glitch");
-
-function glitch(elem, charLen) {
-  const charList = "&#*+%?£@§$";
-  let randomText = "";
-  for (let i = 0; i < charLen; i++) {
-    randomText += charList[Math.floor(Math.random() * charList.length)];
-  }
-  elem.innerText = randomText;
-  setTimeout(glitch, 100, elem, charLen);
-}
-
-for (elem of glitchTexts) {
-  console.log(elem);
-  glitch(elem, elem.innerText.length);
-}
