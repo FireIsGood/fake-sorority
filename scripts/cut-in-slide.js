b753d7c8b41c2df35faa9d326786991fb2a0ee99
FireIsGood <fireisadequate@gmail.com>
2022-09-24 12:31:16 -0700

    Add slide in effect to cards

--- scripts/cut-in-slide.js
+++ scripts/cut-in-slide.js
@@ -1,20 +1,21 @@
-const articles = document.querySelectorAll(".article-slide > article");
+const cutInText = document.querySelectorAll(".left, .right");
 
 // Intersection Observer
 
 const options = {
   root: null,
-  threshold: 0.2,
+  threshold: 0.5,
   rootMargin: "0px",
 };
 
 const observer = new IntersectionObserver(function (entries, observer) {
   entries.forEach((entry) => {
+    console.log(entry);
     entry.isIntersecting ? entry.target.classList.add("show") : null;
   });
 }, options);
 
-for (let article of articles) {
-  article.classList.add("js-active");
-  observer.observe(article);
+for (let text of cutInText) {
+  text.classList.add("js-active");
+  observer.observe(text);
 }
